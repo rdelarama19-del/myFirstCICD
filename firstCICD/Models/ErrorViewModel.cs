de690f5d9682f1ebc802ad6c4edb55d8ea9fcdf8
R Dela Rama  (SBKNeo-IT-DEV) <01PH1696.Raymond.D@ICT888.NET>
2026-04-10 14:45:49 +0800

    first commit

---
+++ firstCICD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace firstCICD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
